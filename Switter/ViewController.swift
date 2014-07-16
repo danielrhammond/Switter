bb80534edc33f71e41134e426a41edb3ec5e8952
Daniel Hammond <danielrhammond@gmail.com>
2014-07-15 23:12:21 -0700

    Initial Commit

---
+++ Switter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Switter
+//
+//  Created by Daniel Hammond on 7/15/14.
+//  Copyright (c) 2014 Daniel Hammond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
