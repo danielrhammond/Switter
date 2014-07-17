78d52078b5cb3f8252007430ba5ba0292cc28cdf
Daniel Hammond <danielrhammond@gmail.com>
2014-07-16 23:50:24 -0700

    init

---
+++ Switter/TwitterClient.swift
@@ -0,0 +1,18 @@
+//
+//  TwitterClient.swift
+//  Switter
+//
+//  Created by Daniel Hammond on 7/15/14.
+//  Copyright (c) 2014 Daniel Hammond. All rights reserved.
+//
+
+import UIKit
+import Social
+import Accounts
+
+class TwitterClient: NSObject {
+    let account: ACAccount
+    init(account: ACAccount) {
+        
+    }
+}
--- Switter/ViewController.swift
+++ Switter/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    let tableView = UITableView()
+    let cellReuseIdentifier = "com.switter.rootViewController.tableView.cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(tableView)
+        
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0.0))
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0.0))
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0.0))
+        view.addConstraint(NSLayoutConstraint(item: tableView, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0.0))
+
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Tableview
+    
 }
 
